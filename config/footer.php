2dd80160df7ec0d7d4b015c738ab8df95176a1fb
cypha-io <cyphauser.main@gmail.com>
2023-03-14 09:29:32 +0000

    New system added #1

---
+++ config/footer.php
@@ -0,0 +1,12 @@
+<footer class="main-footer fixed-bottom">
+    <strong>Copyright &copy; <?php echo date('Y');?> 
+    <a href="./">OLAG Clinic's Patient Management System</a>.</strong> All rights reserved.
+    <div class="float-right d-sm-block">
+      OLAG SHS
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
